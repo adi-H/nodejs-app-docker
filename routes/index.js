038d32e0689388d00736e243d69e39d91979129b
adi-H <adi.haimsohn@gmail.com>
2021-02-15 13:55:51 +0200

    change title 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node.js Express App aaaa' });
+  res.render('index', { title: 'Node.js Express App aaaaaaaaaaaaaa' });
 });
 
 module.exports = router;
